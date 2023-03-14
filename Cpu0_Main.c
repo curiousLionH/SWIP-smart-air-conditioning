13b848666c490f4632ac23151566c1862ba33658
wkjungswip <w.k.jung.swip@gmail.com>
2023-03-14 16:19:25 +0900

    Update Cpu0_Main.c

--- Cpu0_Main.c
+++ Cpu0_Main.c
@@ -28,13 +28,11 @@
 #include "IfxCpu.h"
 #include "IfxScuWdt.h"
 
-IfxCpu_syncEvent g_cpuSyncEvent = 0;
+#include "IfxCcu6_reg.h"
+#include "IfxVadc_reg.h"
+#include "IfxGtm_reg.h"
 
-/* MACRO DEFINE START */
-// GPIO input channel
-#define P10_IOCR0_BASE 0xF003B010
-
-#define PC0_BIT_LSB_IDX         3
+// Port registers
 #define PC1_BIT_LSB_IDX         11
 #define PC2_BIT_LSB_IDX         19
 #define PC3_BIT_LSB_IDX         27
@@ -46,145 +44,384 @@ IfxCpu_syncEvent g_cpuSyncEvent = 0;
 #define P5_BIT_LSB_IDX          5
 #define P7_BIT_LSB_IDX          7
 
-// SCU_EICR register
-#define EXIS0_BIT_LSB_IDX 4
-#define FEN0_BIT_LSB_IDX 8
-#define EIEN0_BIT_LSB_IDX 11
-#define INP0_BIT_LSB_IDX 12
+// SCU registers
+#define LCK_BIT_LSB_IDX         1
+#define ENDINIT_BIT_LSB_IDX     0
+#define EXIS0_BIT_LSB_IDX       4
+#define FEN0_BIT_LSB_IDX        8
+#define EIEN0_BIT_LSB_IDX       11
+#define INP0_BIT_LSB_IDX        12
+#define IGP0_BIT_LSB_IDX        14
 
-// SCU_IGCR register
-#define IGP0_BIT_LSB_IDX 14
+// SRC registers
+#define SRPN_BIT_LSB_IDX        0
+#define TOS_BIT_LSB_IDX         11
+#define SRE_BIT_LSB_IDX         10
 
-// SCUERU register
-#define SRPN_BIT_LSB_IDX 0
-#define SRE_BIT_LSB_IDX 10
-#define TOS_BIT_LSB_IDX 11
+// CCU60 registers
+#define DISS_BIT_LSB_IDX        1
+#define DISR_BIT_LSB_IDX        0
+#define CTM_BIT_LSB_IDX         7
+#define T12PRE_BIT_LSB_IDX      3
+#define T12CLK_BIT_LSB_IDX      0
+#define T12STR_BIT_LSB_IDX      6
+#define T12RS_BIT_LSB_IDX       1
+#define INPT12_BIT_LSB_IDX      10
+#define ENT12PM_BIT_LSB_IDX     7
 
-// etc
-#define UNBOUNCE_TIME 100
-/* MACRO DEFINE END */
+// VADC registers
+#define DISS_BIT_LSB_IDX        1
+#define DISR_BIT_LSB_IDX        0
+#define ANONC_BIT_LSB_IDX       0
+#define ASEN0_BIT_LSB_IDX       24
+#define CSM0_BIT_LSB_IDX        3
+#define PRIO0_BIT_LSB_IDX       0
+#define CMS_BIT_LSB_IDX         8
+#define FLUSH_BIT_LSB_IDX       10
+#define TREV_BIT_LSB_IDX        9
+#define ENGT_BIT_LSB_IDX        0
+#define RESPOS_BIT_LSB_IDX      21
+#define RESREG_BIT_LSB_IDX      16
+#define ICLSEL_BIT_LSB_IDX      0
+#define VF_BIT_LSB_IDX          31
+#define RESULT_BIT_LSB_IDX      0
+#define ASSCH7_BIT_LSB_IDX      7
 
-unsigned char cur_mode = 0x0;
+// GTM registers
+#define DISS_BIT_LSB_IDX            1
+#define DISR_BIT_LSB_IDX            0
+#define SEL7_BIT_LSB_IDX            14
+#define SEL9_BIT_LSB_IDX            18
+#define SEL11_BIT_LSB_IDX           22
+#define EN_FXCLK_BIT_LSB_IDX        22
+#define FXCLK_SEL_BIT_LSB_IDX       0
 
-void initLED(void){
-    P10_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX); /* clear P10_IOCR0.PC1 */
-    P10_IOCR0.U &= ~(0x1F << PC2_BIT_LSB_IDX); /* clear P10_IOCR0.PC2 */
+// GTM - TOM0 registers
+#define UPEN_CTRL1_BIT_LSB_IDX      18
+#define HOST_TRIG_BIT_LSB_IDX       0
+#define ENDIS_CTRL1_BIT_LSB_IDX     2
+#define OUTEN_CTRL1_BIT_LSB_IDX     2
+#define RSCNT0_CN1_BIT_LSB_IDX      18
+#define FUPD_CTRL1_BIT_LSB_IDX      2
+#define CLK_SRC_SR_BIT_LSB_IDX      12
+#define OSM_BIT_LSB_IDX             26
+#define TRIGOUT_BIT_LSB_IDX         24
+#define SL_BIT_LSB_IDX              11
 
-    P10_IOCR0.U |= (0x10 << PC1_BIT_LSB_IDX); /* P10.1(D12) -> output mode */
-    P10_IOCR0.U |= (0x10 << PC2_BIT_LSB_IDX); /* P10.2(D13) -> output mode */
-}
+IfxCpu_syncEvent g_cpuSyncEvent = 0;
 
-void initRGBLED(void){
-    // reset port IOCR register
-    P02_IOCR4.U &= ~(0x1F << PC7_BIT_LSB_IDX); /* clear P02_IOCR4.PC7 */
-    P10_IOCR0.U &= ~(0x1F << PC3_BIT_LSB_IDX); /* clear P10_IOCR0.PC2 */
-    P10_IOCR4.U &= ~(0x1F << PC5_BIT_LSB_IDX); /* clear P10_IOCR0.PC2 */
-
-    // set port as general purpose output
-    P02_IOCR4.U |=  (0x10 << PC7_BIT_LSB_IDX); /* P02.7(D9) -> output mode */
-    P10_IOCR0.U |=  (0x10 << PC3_BIT_LSB_IDX); /* P10.3(D10) -> output mode */
-    P10_IOCR4.U |=  (0x10 << PC5_BIT_LSB_IDX); /* P10.5(D11) -> output mode */
-}
-
-
-void initBTN(void){
-    P02_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX); /* clear P2_IOCR0.PC0 */
-    P02_IOCR0.U |= (0x02 << PC1_BIT_LSB_IDX); /* P02.1(D3) -> general input mode(pull-up) */
-}
-
-<<<<<<< Updated upstream
-void initERU(void){
-    /* ERU Interrupt Setting */
-    // ERS channel 2의  4개 External Request 입력 중, 두번째 인덱스 선택 , -> ln21
-    SCU_EICR1.U &= ~(0x7 << EXIS0_BIT_LSB_IDX); /* clear SCU_EICR1_EXIS */
-    SCU_EICR1.U |= (0x1 << EXIS0_BIT_LSB_IDX); /* SCU_EICR1_EXIS0 -> ERS2_ln21 */
-
-    // Interrupt 발생 조건을 Input Channel의 입력신호 rising/falling edge 감지하도록 설정 -> falling edge
-    SCU_EICR1.U |= (0x1 << FEN0_BIT_LSB_IDX); /* SCU_EICR1_FEN0 -> falling edge detection */
-
-    // falling edge 발생시 Trigger 신호 발생 enable
-    SCU_EICR1.U |= (0x1 << EIEN0_BIT_LSB_IDX); /* SCU_EICR1_EIEN0 -> trigger generate enabled */
-
-    // 생성되는 Trigger 신호를 OGU를 통해 Interrupt 신호로 내보내기 위해 OGU0로 전송
-    SCU_EICR1.U &= ~(0x7 << INP0_BIT_LSB_IDX); /* SCU_EICR_INP0 -> trigger output to OGU0 */
-
-    // OGU에서 입력받은 Trigger 신호를 ERU의 IOUT 포트로 출력하도록 설정.
-    SCU_IGCR0.U &= ~(0x3 << IGP0_BIT_LSB_IDX); /* clear SCU_IGCR0_IGP0 */
-    SCU_IGCR0.U |= (0x1 << IGP0_BIT_LSB_IDX); /* SCU_IGCR0_IGP0 -> output is activated(response to trigger) */
-
-    /* SRC Interrupt Setting */
-    // IOUT interrupt SRC_SCUERU0.
-    // SRC_SCUERU0 priority
-    SRC_SCUERU0.U &= ~(0xFF << SRPN_BIT_LSB_IDX); /* clear Service Request Priority Number */
-    SRC_SCUERU0.U |= (0x0A << SRPN_BIT_LSB_IDX); /* Service Request Priority Number -> 10 */
-
-    SRC_SCUERU0.U &= ~(0x03 << TOS_BIT_LSB_IDX); /* CPU0 service is initiated */
-
-    SRC_SCUERU0.U |= (0x1 << SRE_BIT_LSB_IDX); /* Service Request Enabled */
-}
-
-// priority 10으로 설정, cpu0을 이용하도록 설정
-__interrupt(0x0A) __vector_table(0) /* IGNORE WARNING IN VS CODE! */
-void ERU0_ISR(void){  
-}
+void initLED(void);
+void initButton(void);
+void initERU(void);
+void initCCU60(void);
+void initRGBLED(void);
+void initVADC(void);
+void VADC_startConversion(void);
+unsigned int VADC_readResult(void);
+void initGTM(void);
 
 
-__interrupt(0x0A) __vector_table(0)
-void ERU0_ISR(void)
-{
-    P10_OUT.U ^= 0x1 << P1_BIT_LSB_IDX;
-}
-
-__interrupt(0x0B) __vector_table(0)
-void CCU60_T12_ISR(void)
-{
-    VADC_startConversion();
-    adcResult = VADC_readResult();
-}
+unsigned int adcResult;
 
 __interrupt(0x0C) __vector_table(0)
 void ERU_D2_ISR(void)
 {
     if ((P02_OUT.U & (0x1 << P7_BIT_LSB_IDX)) && (P10_OUT.U & (0x1 << P3_BIT_LSB_IDX)))
     {
-    case 0x0:   // Manual, RGB OFF
-        P02_OUT.U &= ~(0x1 << P7_BIT_LSB_IDX);
-        P10_OUT.U &= ~(0x1 << P3_BIT_LSB_IDX);
+        // initial => Blue (Manual)
+        P10_OUT.U |=  (0x1 << P3_BIT_LSB_IDX);
         P10_OUT.U &= ~(0x1 << P5_BIT_LSB_IDX);
-        break;
-    case 0x1:   // SMART, GREEN
-        P02_OUT.U &= ~(0x1 << P7_BIT_LSB_IDX);
-        P10_OUT.U &= ~(0x1 << P3_BIT_LSB_IDX);
-        P10_OUT.U |= (0x1 << P5_BIT_LSB_IDX);
-        break;
-    
-    default:
-        break;
+        P02_OUT.U &= ~(0x1 << P7_BIT_LSB_IDX); 
+    }
+    else
+    {
+        // toggle ( Manual <=> Auto )
+        P10_OUT.U ^= 0x1 << P3_BIT_LSB_IDX;
+        P02_OUT.U ^= (0x1 << P7_BIT_LSB_IDX);
     }
 }
+
+__interrupt(0x0A) __vector_table(0)
+void ERU0_ISR(void)
+{
+    P10_OUT.U ^= 0x1 << P1_BIT_LSB_IDX; // toggle P10.1 (LED D12 RED)
+}
+
+__interrupt(0x0B) __vector_table(0)
+void CCU60_T12_ISR(void)
+{
+    // P10_OUT.U ^= 0x1 << P2_BIT_LSB_IDX;  // toggle P10.2 (LED D13 BLUE)
+
+    VADC_startConversion();
+    adcResult = VADC_readResult();
+}
+
 
 int core0_main(void)
 {
     IfxCpu_enableInterrupts();
-
+    
     /* !!WATCHDOG0 AND SAFETY WATCHDOG ARE DISABLED HERE!!
      * Enable the watchdogs and service them periodically if it is required
      */
     IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
     IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
-
+    
     /* Wait for CPU sync event */
     IfxCpu_emitEvent(&g_cpuSyncEvent);
     IfxCpu_waitEvent(&g_cpuSyncEvent, 1);
-
-    initLED();  /* configure P10.1, P10.2 -> output mode */
+    
+    initERU();
+    initCCU60();
+    initLED();
     initRGBLED();
-    initBTN();  /* configure P2.1 -> general input mode(pull-up) */
-    initERU();  /* init Interrupt Module */
+    initVADC();
+    initGTM();
+    initButton();
 
+    unsigned short duty = 0;
+    GTM_TOM0_TGC0_GLB_CTRL.U |= 0x1 << HOST_TRIG_BIT_LSB_IDX;       // trigger update request signal
+    // GTM_TOM0_TGC1_GLB_CTRL.U |= 0x1 << HOST_TRIG_BIT_LSB_IDX;       // trigger update request signal
+    
     while(1)
-    {
+    {    
+        // Manual Mode
+        if ( P10_OUT.U & (0x1 << P3_BIT_LSB_IDX) )
+        {
+            duty = 12500 * adcResult / 4096;
+            GTM_TOM0_CH1_SR1.U = duty;
+        }
+        
+        // Auto Mode
+        else
+        {
+            continue;
+        }
     }
-
     return (1);
+}
+
+void initLED(void)
+{
+    P10_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX);     // reset P10_IOCR0 PC1
+    P10_IOCR0.U &= ~(0x1F << PC2_BIT_LSB_IDX);     // reset P10_IOCR0 PC2
+
+    P10_IOCR0.U |= 0x10 << PC1_BIT_LSB_IDX;        // set P10.1 push-pull general output
+    P10_IOCR0.U |= 0x10 << PC2_BIT_LSB_IDX;        // set P10.2 push-pull general output
+}
+
+void initButton(void)
+{
+    P02_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX);       // reset P02_IOCR0 PC1
+
+    P02_IOCR0.U |= 0x02 << PC1_BIT_LSB_IDX;          // set P02.1 general input (pull-up connected)
+}
+
+void initERU(void)
+{
+    // ERU setting
+    SCU_EICR1.U &= ~(0x7 << EXIS0_BIT_LSB_IDX);
+    SCU_EICR1.U |= (0x1  << EXIS0_BIT_LSB_IDX);
+
+    SCU_EICR1.U |= 0x1   << FEN0_BIT_LSB_IDX;
+
+    // SCU_EICR1.U |= 0x1 << RENO_BIT_LSB_IDX; // rising edge
+
+    SCU_EICR1.U |= 0x1   << EIEN0_BIT_LSB_IDX;
+
+    SCU_EICR1.U &= ~(0x7 << INP0_BIT_LSB_IDX);
+
+    SCU_IGCR0.U &= ~(0x3 << IGP0_BIT_LSB_IDX);
+    SCU_IGCR0.U |= 0x1   << IGP0_BIT_LSB_IDX;
+
+    // SRC Interrupt setting
+    SRC_SCU_SCU_ERU0.U &= ~(0xFF << SRPN_BIT_LSB_IDX);
+    SRC_SCU_SCU_ERU0.U |= 0x0C   << SRPN_BIT_LSB_IDX;
+
+    SRC_SCU_SCU_ERU0.U &= ~(0x3  << TOS_BIT_LSB_IDX);
+
+    SRC_SCU_SCU_ERU0.U |= 1      << SRE_BIT_LSB_IDX;
+}
+
+void initCCU60(void)
+{
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to clear ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) & ~(1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    CCU60_CLC.U &= ~(1 << DISR_BIT_LSB_IDX);    // enable CCY
+
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to set ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+
+    // CCU60 T12 configurations
+    while((CCU60_CLC.U & (1 << DISS_BIT_LSB_IDX)) != 0);// wait until CCU60 module enabled
+
+    CCU60_TCTR0.U &= ~(0x7 << T12CLK_BIT_LSB_IDX);      // f_T12 = f_CCU6 / prescaler
+    CCU60_TCTR0.U |= 0x2 << T12CLK_BIT_LSB_IDX;         // f_CCU6 = 50 MHz, prescaler = 1024
+    CCU60_TCTR0.U |= 0x1 << T12PRE_BIT_LSB_IDX;         // f_T12 = 48,828 Hz
+
+    CCU60_TCTR0.U &= ~(0x1 << CTM_BIT_LSB_IDX);         // T12 auto reset when period match (PM) occur
+
+
+    CCU60_T12PR.U = 48828 -1;                       // PM interrupt freq. = f_T12 / (T12PR + 1)
+    CCU60_TCTR4.U |= 0x1 << T12STR_BIT_LSB_IDX;         // load T12PR from shadow register
+
+    CCU60_T12.U = 0;                                // clear T12 counter register
+
+
+    // CCU60 T12 PM interrupt setting
+    CCU60_INP.U &= ~(0x3 << INPT12_BIT_LSB_IDX);        // service request output SR0 selected
+    CCU60_IEN.U |= 0x1 << ENT12PM_BIT_LSB_IDX;          // enable T12 PM interrupt
+
+
+    // SRC setting for CCU60
+    SRC_CCU6_CCU60_SR0.U &= ~(0xFF << SRPN_BIT_LSB_IDX);
+    SRC_CCU6_CCU60_SR0.U |= 0x0B << SRPN_BIT_LSB_IDX;   // set priority 0x0B
+
+    SRC_CCU6_CCU60_SR0.U &= ~(0x3 << TOS_BIT_LSB_IDX);  // CPU0 service T12 PM interrupt
+
+    SRC_CCU6_CCU60_SR0.U |= 0x1 << SRE_BIT_LSB_IDX;     // SR0 enabled
+
+
+    // CCU60 T12 counting start
+    CCU60_TCTR4.U = 0x1 << T12RS_BIT_LSB_IDX;           // T12 start counting
+}
+
+void initRGBLED(void)
+{
+    // reset Port IOCR register
+    P02_IOCR4.U &= ~(0x1F << PC7_BIT_LSB_IDX);
+    P10_IOCR4.U &= ~(0x1F << PC5_BIT_LSB_IDX);
+    P10_IOCR0.U &= ~(0x1F << PC3_BIT_LSB_IDX);
+
+    // set Port as general purpose output (push-pull)
+    P02_IOCR4.U |= 0x10 << PC7_BIT_LSB_IDX;
+    P10_IOCR4.U |= 0x10 << PC5_BIT_LSB_IDX;
+    P10_IOCR0.U |= 0x10 << PC3_BIT_LSB_IDX;
+
+    // initialization (white)
+    P02_OUT.U |= 0x1 << P7_BIT_LSB_IDX;
+    P10_OUT.U |= 0x1 << P5_BIT_LSB_IDX;
+    P10_OUT.U |= 0x1 << P3_BIT_LSB_IDX;
+}
+
+void initVADC(void)
+{
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to clear ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) & ~(1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    VADC_CLC.U &= ~(1 << DISR_BIT_LSB_IDX);    // enable VADC
+
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to set ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);
+
+
+    // VADC configurations
+    while((VADC_CLC.U & (1 << DISS_BIT_LSB_IDX)) != 0); // wait until VADC module enabled
+
+    VADC_G4_ARBPR.U |= 0x3 << PRIO0_BIT_LSB_IDX;         // highest priority for Request Source 0
+    VADC_G4_ARBPR.U &= ~(0x1 << CSM0_BIT_LSB_IDX);       // Wait-for-Start Mode
+    VADC_G4_ARBPR.U |= 0x1 << ASEN0_BIT_LSB_IDX;         // Arbitration Source Input 0 Enable
+
+    VADC_G4_QMR0.U &= ~(0x3 << ENGT_BIT_LSB_IDX);
+    VADC_G4_QMR0.U |= 0x1 << ENGT_BIT_LSB_IDX;          // enable conversion request
+    VADC_G4_QMR0.U |= 0x1 << FLUSH_BIT_LSB_IDX;         // clear ADC queue
+
+    VADC_G4_ARBCFG.U |= 0x3 << ANONC_BIT_LSB_IDX;       // ADC normal operation
+
+    VADC_G4_ICLASS0.U &= ~(0x7 << CMS_BIT_LSB_IDX);     // Class 0 Standard Conversion (12-bit)
+
+
+    // VADC Group 4 Channel 7 configuration
+    VADC_G4_CHCTR7.U |= 0x1 << RESPOS_BIT_LSB_IDX;      // result right-aligned
+    VADC_G4_CHCTR7.U &= ~(0xF << RESREG_BIT_LSB_IDX);   // store result @ Result Register G4RES0
+    VADC_G4_CHCTR7.U &= ~(0x3 << ICLSEL_BIT_LSB_IDX);   // Class 0
+
+    VADC_G4_CHASS.U |= 0x1 << ASSCH7_BIT_LSB_IDX;
+}
+
+void VADC_startConversion(void)
+{
+    VADC_G4_QINR0.U |= 0x07;                    // no. of Request Channel = 7
+
+    VADC_G4_QMR0.U |= 0x1 << TREV_BIT_LSB_IDX;  // Generate Conversion Start Trigger Event
+}
+
+unsigned int VADC_readResult(void)
+{
+    unsigned int result;
+
+    while( (VADC_G4_RES0.U & (0x1 << VF_BIT_LSB_IDX)) == 0 );   // wait until read available
+    result = VADC_G4_RES0.U & (0xFFF << RESULT_BIT_LSB_IDX);    // read ADC value
+
+    return result;
+}
+
+
+void initGTM(void)
+{
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to clear ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) & ~(1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    GTM_CLC.U &= ~(1 << DISR_BIT_LSB_IDX);    // enable GTM
+
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to set ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);
+
+    while((GTM_CLC.U & (1 << DISS_BIT_LSB_IDX)) != 0); // wait until GTM module enabled
+
+
+    // GTM clock configuration
+    GTM_CMU_FXCLK_CTRL.U &= ~(0xF << FXCLK_SEL_BIT_LSB_IDX);        // input clock of CMU_FXCLK --> CMU_GCLK_EN
+    GTM_CMU_CLK_EN.U |= 0x2 << EN_FXCLK_BIT_LSB_IDX;                // enable all CMU_FXCLK
+
+    // GTM TOM0 PWM configuration
+    GTM_TOM0_TGC0_GLB_CTRL.U |= 0x2 << UPEN_CTRL1_BIT_LSB_IDX;      // TOM channel 1 update enable
+
+    GTM_TOM0_TGC0_ENDIS_CTRL.U |= 0x2 << ENDIS_CTRL1_BIT_LSB_IDX;   // enable channel 1 on update trigger
+
+    GTM_TOM0_TGC0_OUTEN_CTRL.U |= 0x2 << OUTEN_CTRL1_BIT_LSB_IDX;   // enable channel 1 output on update trigger
+
+    GTM_TOM0_CH1_CTRL.U |= 0x1 << SL_BIT_LSB_IDX;                   // high signal level for duty cycle
+    GTM_TOM0_CH1_CTRL.U |= 0x1 << CLK_SRC_SR_BIT_LSB_IDX;           // clock source --> CMU_FXCLK(1) = 6250 kHz
+    GTM_TOM0_CH1_CTRL.U &= ~(0x1 << OSM_BIT_LSB_IDX);               // continuous mode enable
+    GTM_TOM0_CH1_CTRL.U &= ~(0x1 << TRIGOUT_BIT_LSB_IDX);           // TRIG[x] = TRIG[x-1]
+
+    GTM_TOM0_CH1_SR0.U = 12500 - 1;                                 // PWM freq. = 6250 kHz / 12500 = 500 Hz
+
+    GTM_TOM0_CH1_SR1.U = 1250 - 1;                                  // duty cycle = 1250 / 12500 = 10 % (temporary)
+
+    GTM_TOUTSEL6.U &= ~(0x3 << SEL7_BIT_LSB_IDX);                   // TOUT103 --> TOM0 channel 1
+                                                                    // 103 = 16 * 6 + 7
 }
